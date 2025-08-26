de84b38c2850fb68e2d8cba7958e878017a0fc05
2hnK <kimgh921@gmail.com>
2025-08-26 16:24:50 +0900

    initializing project

---
+++ src/test/java/com/hun2/demoproject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hun2.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
